d843508e1462feadf65392931b7837181a3ce1c0
yasar-98 <yasar_98@outlook.sa>
2021-06-09 01:50:40 +0300

    java first program

---
+++ Fundemental/javaFun/FirstJavaProgram.java
@@ -0,0 +1,10 @@
+public class FirstJavaProgram{
+    public static void main(String[] args) {
+	int age=23;
+	String name="Yasar Naser";
+	String hometown="Jenin";
+        System.out.printf("My name iss "+ name +
+	" I am "+ age + " years old " +
+	"My hometown is "+ hometown);
+    }
+}
